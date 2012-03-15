8b24c297bf25c3adda195aa19ddf65d0b72294c9
justin <juzten@gmail.com>
2012-03-15 00:01:24 -0400

    started test_for_github

---
+++ test_for_github/src/group/four/test_for_github/Test_for_githubActivity.java
@@ -0,0 +1,13 @@
+package group.four.test_for_github;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test_for_githubActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
